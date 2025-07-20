65f364a4a291c3bf3db028332c6d97c70dca6c08
zZDarkss <Fabriciosa2610@gmail.com>
2025-07-20 04:21:51 -0300

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVHcire7EkFqZiIl05DdMFhwXpT4KY18A",
+  authDomain: "controle-de-vagas-8b628.firebaseapp.com",
+  projectId: "controle-de-vagas-8b628",
+  storageBucket: "controle-de-vagas-8b628.firebasestorage.app",
+  messagingSenderId: "861426269071",
+  appId: "1:861426269071:web:ecd1d313b5dbc66d9069fa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
